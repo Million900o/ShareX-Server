3f0e4a161aa5b7e560dc6cb5b12767d7ff98d999
Million900o <apaolini900o@gmail.com>
2020-12-21 18:21:53 -0500

    subdomain check

--- src/routes/api/user/subdomain.js
+++ src/routes/api/user/subdomain.js
@@ -13,12 +13,14 @@ router.post('/api/user/subdomain', async (req, res) => {
     bcrypt.compare(password, req.session.userData.authentication.password).then(async e => {
       if (e) {
         const domain = await req.app.server.models.DomainModel.findOne({ domain: req.session.userData.domain.domain });
-        domain.subdomains[subdomain] = req.session.userData.id;
-        delete domain.subdomains[req.session.userData.domain.subdomain];
-        await req.app.server.models.DomainModel.updateOne({ domain: req.session.userData.domain.domain }, domain);
-        await req.app.server.models.UserModel.updateOne({ id: req.session.userData.id }, { 'domain.subdomain': subdomain });
-        req.session.userData = await req.app.server.models.UserModel.findOne({ id: req.session.userData.id });
-        res.redirect('/dashboard?page=subdomain&success=Subdomain successfully updated to: ' + subdomain + '.');
+        if(!domain.subdomains[subdomain]) {
+          domain.subdomains[subdomain] = req.session.userData.id;
+          delete domain.subdomains[req.session.userData.domain.subdomain];
+          await req.app.server.models.DomainModel.updateOne({ domain: req.session.userData.domain.domain }, domain);
+          await req.app.server.models.UserModel.updateOne({ id: req.session.userData.id }, { 'domain.subdomain': subdomain });
+          req.session.userData = await req.app.server.models.UserModel.findOne({ id: req.session.userData.id });
+          res.redirect('/dashboard?page=subdomain&success=Subdomain successfully updated to: ' + subdomain + '.');
+        } else res.redirect('/dashboard?page=subdomain&error=Subdomain is already taken.');
       } else res.redirect('/dashboard?page=subdomain&error=Incorrect password.');
       return;
     });
